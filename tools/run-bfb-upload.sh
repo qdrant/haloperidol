7473b6a5e427c02e7cc76d16b85ce1309662d2ba
Roman Titov <ffuugoo@users.noreply.github.com>
2023-07-25 13:27:04 +0200

    Add path to the `run-remote.sh` to the `run-bfb-upload.sh`

--- tools/run-bfb-upload.sh
+++ tools/run-bfb-upload.sh
@@ -9,9 +9,11 @@ function self {
 }
 
 declare SELF="$(self)"
-declare ROOT="$(dirname "$SELF")"
-declare HETZNER="$ROOT/clouds/hetzner"
 
+declare ROOT="$(dirname "$SELF")"
+declare RUN_REMOTE="$ROOT/run_remote.sh"
+
+declare HETZNER="$ROOT/clouds/hetzner"
 declare GET_PRIVATE_IP="$HETZNER/get_private_ip.sh"
 
 
@@ -19,6 +21,7 @@ declare QDRANT_HOSTS=()
 
 for IDX in $(seq 3)
 do
+    echo qdrant-node-"$IDX"
     QDRANT_HOSTS+=( "$("$GET_PRIVATE_IP" qdrant-node-"$IDX")" )
 done
 
