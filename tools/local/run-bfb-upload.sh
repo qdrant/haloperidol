3dcaa87ce759a2cb5443a3c88ab1ac1a64a1822f
generall <andrey@vasnetsov.com>
2023-07-25 13:20:23 +0200

    add api key to bfb

--- tools/local/run-bfb-upload.sh
+++ tools/local/run-bfb-upload.sh
@@ -7,6 +7,7 @@ QDRANT_HOSTS=( $QDRANT_HOSTS ) # Unescaped parameter expansion
 BFB_CONTAINER_NAME="bfb-upload"
 BFB_IMAGE_NAME="qdrant/bfb:latest"
 
+QDRANT_API_KEY=${QDRANT_API_KEY:-""}
 
 QDRANT_URIS=( ${QDRANT_HOSTS/#/--uri } ) # Unescaped parameter expansion
 
@@ -39,6 +40,7 @@ docker run \
     -it \
     --network host \
     --name ${BFB_CONTAINER_NAME} \
+    -e QDRANT_API_KEY=${QDRANT_API_KEY} \
     ${BFB_IMAGE_NAME} \
     ./bfb ${BFB_PARAMETERS}
 
