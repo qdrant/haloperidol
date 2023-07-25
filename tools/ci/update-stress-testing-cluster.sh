083e624d6022287101bd0845630a61fdbe992b92
Roman Titov <ffuugoo@users.noreply.github.com>
2023-07-25 12:57:01 +0200

    WIP: Refactor `update-stress-testing-cluster.sh`

--- tools/ci/update-stress-testing-cluster.sh
+++ tools/ci/update-stress-testing-cluster.sh
@@ -13,43 +13,28 @@ declare SELF="$(self)"
 declare CI="$(dirname "$SELF")"
 
 declare ROOT="$(dirname "$CI")"
+declare RUN_QDRANT_CLUSTER="$ROOT/run_qdrant_cluster.sh"
 declare RUN_REMOTE="$ROOT/run_remote.sh"
 
 declare HETZNER="$ROOT/clouds/hetzner"
 declare GET_PRIVATE_IP="$HETZNER/get_private_ip.sh"
 
 declare LOCAL="$ROOT/local"
-declare RUN_QDRANT_NODE="$LOCAL/run-qdrant-node.sh"
 declare RUN_BFB_UPLOAD="$LOCAL/run-bfb-upload.sh"
 
 
 function update-qdrant-nodes {
-	declare BOOTSTRAP=''
-
-	for IDX in $(seq 3)
-	do
-		declare NODE=qdrant-node-"$IDX"
-
-		declare PRIV_ADDR; PRIV_ADDR="$("$GET_PRIVATE_IP" "$NODE")"
-
-		ENV_CONTEXT="${BOOTSTRAP-}" \
-		RUN_SCRIPT="$RUN_QDRANT_NODE" \
-		SERVER_NAME="$NODE" \
-		"$RUN_REMOTE"
-
-		if [[ ! "${BOOTSTRAP-}" ]]
-		then
-			BOOTSTRAP="--bootstrap $ADDR"
-		fi
-
-		if declare -a QDRANT_HOSTS &>/dev/null
-		then
-			QDRANT_HOSTS+=( "$PRIV_ADDR" )
-		fi
-	done
+	"$RUN_QDRANT_CLUSTER"
 }
 
 function update-bfb-upload {
+	declare QDRANT_HOSTS=()
+
+	for IDX in "$(seq 3)"
+	do
+		QDRANT_HOSTS+=( "$("$GET_PRIVATE_IP" qdrant-node-"$IDX")" )
+	done
+
 	ENV_CONTEXT="QDRANT_HOSTS='${QDRANT_HOSTS[@]}'" \
 	RUN_SCRIPT="$RUN_QDRANT_NODE" \
 	SERVER_NAME=qdrant-bfb-upload \
@@ -57,6 +42,5 @@ function update-bfb-upload {
 }
 
 
-declare QDRANT_HOSTS=()
 update-qdrant-nodes
 update-bfb-upload
