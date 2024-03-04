1a72050ccb1be36c444aae875e9fd8201275d42a
KShivendu <kshivendu1@gmail.com>
2024-03-04 20:35:40 +0530

    refactor: Create function to insert into PSQL values

--- tools/local/collect-stats.sh
+++ tools/local/collect-stats.sh
@@ -11,24 +11,44 @@ QDRANT_URIS=( ${QDRANT_URIS[@]/%/:6333} )
 
 PSQL_VALUES=""
 
+# function to insert to PSQL_VALUES:
+function insert_to_psql_values {
+    local uri=$1
+    local version=$2
+    local commit_id=$3
+    local num_vectors=$4
+    local num_snapshots=$5
+    local measure_timestamp=$6
+
+    if [ -n "$PSQL_VALUES" ]; then
+        # If there are already values, add a comma
+        PSQL_VALUES+=" ,"
+    fi
+
+    PSQL_VALUES+=" ('$uri', '$version', '$commit_id', $num_vectors, $num_snapshots, '$measure_timestamp')"
+}
+
+
 for uri in "${QDRANT_URIS[@]}"; do
     echo "$uri"
 
     root_api_response=$(curl --url "$uri/" --header "api-key: $QDRANT_API_KEY")
+
     if ! (echo "$root_api_response" | jq -e '.'); then
         # Node is down
-        root_api_response='{}'
+        insert_to_psql_values "$uri" "null" "null" 0 0 "$NOW"
+        continue
     fi
 
-    version=$(echo "$root_api_response" | jq -r '.version // "null"')
+    version=$(echo "$root_api_response" | jq -r '.version')
 
-    commit_id=$(echo "$root_api_response" | jq -r '.commit // "null"')
+    commit_id=$(echo "$root_api_response" | jq -r '.commit')
 
     num_vectors=$(curl --request POST \
         --url "$uri/collections/benchmark/points/count" \
         --header "api-key: $QDRANT_API_KEY" \
         --header 'content-type: application/json' \
-        --data '{"exact": true}' | jq -r '.result.count // 0')
+        --data '{"exact": true}' | jq -r '.result.count')
 
     num_snapshots=$(curl --request GET \
         --url "$uri/collections/benchmark/snapshots" \
@@ -36,12 +56,8 @@ for uri in "${QDRANT_URIS[@]}"; do
         --header 'content-type: application/json' \
         | jq -r '.result[].name | length')
 
-    if [ -n "$PSQL_VALUES" ]; then
-        # If there are already values, add a comma
-        PSQL_VALUES+=" ,"
-    fi
 
-    PSQL_VALUES+=" ('$uri', '$version', '$commit_id', $num_vectors, $num_snapshots, '$NOW')"
+    insert_to_psql_values "$uri" "$version" "$commit_id" "$num_vectors" "$num_snapshots" "$NOW"
 
     sleep 1
 done
