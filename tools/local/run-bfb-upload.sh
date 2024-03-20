748df40ae56a4b9cc7635b74f418bc4e78b1f432
Kumar Shivendu <kshivendu1@gmail.com>
2024-03-20 17:31:07 +0530

    Revert "fix: Collection should use 6 shards with replication factor = 1"

--- tools/local/run-bfb-upload.sh
+++ tools/local/run-bfb-upload.sh
@@ -12,8 +12,7 @@ QDRANT_URIS=( ${QDRANT_URIS[@]/%/:6334} )
 
 BFB_PARAMETERS=" \
     ${QDRANT_URIS[@]/#/--uri } \
-    --replication-factor 1 \
-    --shards 6
+    --replication-factor 2 \
     --keywords 10 \
     --dim 768 \
     -n 1000000000 \
