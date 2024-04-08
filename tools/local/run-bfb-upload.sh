32dd3fbc54503f261693756b0a24145139b2199b
Kumar Shivendu <kshivendu1@gmail.com>
2024-04-08 18:39:22 +0530

    feat: Run bfb upload sequentially till 200K

--- tools/local/run-bfb-upload.sh
+++ tools/local/run-bfb-upload.sh
@@ -16,7 +16,7 @@ BFB_PARAMETERS=" \
     --shards 1
     --keywords 10 \
     --dim 768 \
-    -n 1000000000 \
+    -n 200000 \
     --batch-size 100 \
     --threads 1 \
     --parallel 1 \
@@ -25,7 +25,6 @@ BFB_PARAMETERS=" \
     --quantization scalar \
     --timing-threshold 1 \
     --on-disk-vectors true \
-    --max-id 20000 \
     --delay 200 \
     --timeout 30
 "
