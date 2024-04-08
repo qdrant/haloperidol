79181112fec432acb018bb2ca819e7757f577808
Kumar Shivendu <kshivendu1@gmail.com>
2024-04-08 17:52:08 +0530

    feat: Replicate the same shard 3 times using bfb upload (#40)

--- tools/local/run-bfb-upload.sh
+++ tools/local/run-bfb-upload.sh
@@ -12,7 +12,8 @@ QDRANT_URIS=( ${QDRANT_URIS[@]/%/:6334} )
 
 BFB_PARAMETERS=" \
     ${QDRANT_URIS[@]/#/--uri } \
-    --replication-factor 2 \
+    --replication-factor 3 \
+    --shards 1
     --keywords 10 \
     --dim 768 \
     -n 1000000000 \
