8bf594a15bdaaf8e3eb28aab8adb79e710a9a245
KShivendu <kshivendu1@gmail.com>
2024-04-18 14:14:57 +0530

    feat: Run collect stats script on remote server

---
+++ tools/run-collect-stats.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+function self {
+    realpath "${BASH_SOURCE[0]}" || which "${BASH_SOURCE[0]}"
+    return "$?"
+}
+
+declare SELF="$(self)"
+
+declare ROOT="$(dirname "$SELF")"
+declare RUN_REMOTE="$ROOT/run_remote.sh"
+
+declare LOCAL="$ROOT/local"
+declare RUN_COLLECT_STATS="$LOCAL/run-collect-stats.sh"
+
+HCLOUD_TOKEN=${HCLOUD_TOKEN:-""}
+QDRANT_CLUSTER_URL=${QDRANT_CLUSTER_URL:-""}
+QDRANT_API_KEY=${QDRANT_API_KEY:-""}
+POSTGRES_HOST=${POSTGRES_HOST:-""}
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-""}
+
+QDRANT_HOSTS=()
+for IDX in {0..4}; do
+    QDRANT_HOSTS+=("node-${IDX}-${QDRANT_CLUSTER_URL}")
+done
+
+SERVER_NAME=${SERVER_NAME:-"qdrant-manager"}
+
+ENV_CONTEXT="${HCLOUD_TOKEN@A} ${QDRANT_CLUSTER_URL@A} ${QDRANT_API_KEY@A} ${POSTGRES_HOST@A} ${POSTGRES_PASSWORD@A} ${QDRANT_HOSTS[@]@A}" \
+RUN_SCRIPT="$RUN_COLLECT_STATS" \
+SERVER_NAME="$SERVER_NAME" \
+BG_TASK_NAME="collect-stats" \
+bash -x "$RUN_REMOTE"
