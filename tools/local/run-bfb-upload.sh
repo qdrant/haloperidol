522fc86d756cf2626f9ea86a694bb516cfb990f4
Kumar Shivendu <kshivendu1@gmail.com>
2024-04-23 14:37:54 +0530

    feat: Log bfb output to file with ts

--- tools/local/run-bfb-upload.sh
+++ tools/local/run-bfb-upload.sh
@@ -41,9 +41,10 @@ docker run \
     -d \
     --network host \
     --name ${BFB_CONTAINER_NAME} \
+    -v $PWD/bfb-upload.log:./bfb-upload.log
     -e QDRANT_API_KEY=${QDRANT_API_KEY} \
     ${BFB_IMAGE_NAME} \
-    ./bfb ${BFB_PARAMETERS}
+    ./bfb ${BFB_PARAMETERS} 2>&1 | tee >(awk '{print strftime("[%Y-%m-%d %H:%M:%S]"), $0}' >>  ./bfb-upload.log 2>&1
 
 sleep 5
 
