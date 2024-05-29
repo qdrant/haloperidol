44d352d1eb01ee63c45e7e68cbb903897837e010
Kumar Shivendu <kshivendu1@gmail.com>
2024-05-29 14:49:40 +0530

    feat: Improve snapshot cron (#51)
    * feat: Improve snapshot cron
    
    * feat: Isolate set -x
    
    * fix: Remove set -x
    
    * fix: Run in foreground
    
    * fix: Remove sleep
    
    * feat: Skip if snapshot already exists
    
    * feat: Run in remote server
    
    * feat: Silent and fail with body
    
    * feat: Forward all logs to a dedicated file
    
    * feat: List screens before clearning them
    
    * feat: Create snapshots with wait of 1hr
    
    * feat: Add random sleep
    
    * feat: Add structured logs and avoid crashing on error
    
    * feat: Use logfmt format for logging

--- tools/local/run-snapshots.sh
+++ tools/local/run-snapshots.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-set -euo pipefail
+set -uo pipefail
 
+# set -x
 
 QDRANT_API_KEY=${QDRANT_API_KEY:-""}
 
@@ -10,22 +11,67 @@ QDRANT_COLLECTION_NAME=${QDRANT_COLLECTION_NAME:-"benchmark"}
 QDRANT_URIS=( ${QDRANT_HOSTS[@]/#/https://} )
 QDRANT_URIS=( ${QDRANT_URIS[@]/%/:6333} )
 
-function create_and_delete_snapshot() {
-    curl -X POST -H "api-key: ${QDRANT_API_KEY}" "$1/collections/${QDRANT_COLLECTION_NAME}/snapshots"
+log_with_timestamp() {
+    while IFS= read -r line; do
+        echo "timestamp=$(date --rfc-3339=seconds --utc) $line"
+    done
+}
+# Redirect stdout (1) and stderr (2) to a log file
+exec > >(log_with_timestamp >> /var/log/run-snapshots-cron.log) 2>&1
 
-    curl -X GET  -H "api-key: ${QDRANT_API_KEY}" "$1/collections/${QDRANT_COLLECTION_NAME}/snapshots" -s \
-        | jq -r .result[].name \
-        | xargs -I {} curl -X DELETE -H "api-key: ${QDRANT_API_KEY}" "$1/collections/${QDRANT_COLLECTION_NAME}/snapshots/{}"
+function create_snapshot() {
+    snapshot_count=$(get_snapshot_count "$1")
+    if [ "$snapshot_count" -gt 0 ]; then
+        # This exists to avoid OOD errors
+        echo "msg=\"snapshots already exist\" operation=create_snapshot action=skip snapshot_count=$snapshot_count url=$1"
+        return
+    fi
+
+    snapshot=$(curl -s --fail-with-body -X POST -H "api-key: ${QDRANT_API_KEY}" "$1/collections/${QDRANT_COLLECTION_NAME}/snapshots" | jq -r ".result.name")
+    echo "msg=\"created snapshot\" operation=create_snapshot action=create snapshot_name=\"$snapshot\" url=$1"
 }
 
+function delete_snapshots() {
+    snapshot_names=$(curl -s --fail-with-body -X GET  -H "api-key: ${QDRANT_API_KEY}" "$1/collections/${QDRANT_COLLECTION_NAME}/snapshots" -s \
+        | jq -r ".result[].name")
+    echo "operation=delete_snapshots action=fetch_snapshots snapshots=\"$snapshot_names\" url=$1"
+    echo "$snapshot_names" | xargs -I {} curl -s --fail-with-body -X DELETE -H "api-key: ${QDRANT_API_KEY}" "$1/collections/${QDRANT_COLLECTION_NAME}/snapshots/{}"
+    echo "operation=delete_snapshots action=delete snapshots=$snapshot_names url=$1"
+}
+
+function get_snapshot_count() {
+    curl -s --fail-with-body -X GET -H "api-key: ${QDRANT_API_KEY}" "$1/collections/${QDRANT_COLLECTION_NAME}/snapshots" -s \
+    | jq -r ".result[].name" \
+    | wc -l
+}
 
 function run_in_loop() {
+    # Create snapshots
     for url in "${QDRANT_URIS[@]}"; do
-        create_and_delete_snapshot "$url" || true
-        sleep 60
+        create_snapshot "$url" || true
+    done
+
+    # Delete snapshots
+    for url in "${QDRANT_URIS[@]}"; do
+        delete_snapshots "$url" || true
+        sleep 10
+        snapshot_count=$(get_snapshot_count "$url" || "-1")
+        echo "operation=get_snapshot_count snapshot_count=$snapshot_count"
+        if [ "$snapshot_count" -ne 0 ]; then
+            echo "ERROR: Snapshot count is $snapshot_count (!= 0)"
+            echo "::set-output name=failed::true"
+        fi
     done
 }
 
+while true; do
+  echo "==================="
+  echo "msg=\"Starting snapshot cron\""
 
-run_in_loop > output-snapshots.log 2>&1 &
-
+  run_in_loop # > output-snapshots.log 2>&1 &
+  # generate a time (seconds) between 30mins and 1 hour:
+  delay=$((RANDOM % 1800 + 30 * 60))
+  # Convert to minutes:
+  echo "msg=\"Sleeping\" duration_m=$(( delay / 60 ))"
+  sleep $delay
+done
--- tools/run_remote.sh
+++ tools/run_remote.sh
@@ -41,6 +41,6 @@ if [ -z "$BG_TASK_NAME" ]; then
 else
     # Could directly cat inside screen instead of using scp but it's better to persist the script for debugging purposes
     scp -oStrictHostKeyChecking=no "$RUN_SCRIPT" "$SSH_USER@$SERVER_IP:/tmp/$BG_TASK_NAME.sh"
-    ssh -oStrictHostKeyChecking=no "$SSH_USER@$SERVER_IP" "screen -X -S $BG_TASK_NAME quit || true" # Kill existing screen session
+    ssh -oStrictHostKeyChecking=no "$SSH_USER@$SERVER_IP" "screen -list; screen -X -S $BG_TASK_NAME quit || true" # Kill existing screen session
     ssh -oStrictHostKeyChecking=no "$SSH_USER@$SERVER_IP" "screen -dmS $BG_TASK_NAME bash -c 'echo \"$ENV_CONTEXT\" | cat - /tmp/$BG_TASK_NAME.sh | bash -x'"
 fi
