c051e3f83dc7146393780bb51c919664ad30c9b3
KShivendu <kshivendu1@gmail.com>
2025-03-06 11:41:34 +0530

    set -x in postgres

--- tools/local/collect-node-metrics.sh
+++ tools/local/collect-node-metrics.sh
@@ -245,7 +245,9 @@ fi
 
 function postgres_query() {
     sql_query=$1
+    set -x
     docker run --rm --name $POSTGRES_CLIENT_CONTAINER_NAME jbergknoff/postgresql-client "$POSTGRES_URL" -c "$sql_query"
+    set +x
 }
 
 # Read search results from json file and upload it to postgres
